4ae5aa7b371f0494a5429e33899baea33bbfb6fc
AsadPug <97375660+AsadPug@users.noreply.github.com>
2024-04-28 21:27:01 -0400

    level select functionnal (can start levels + UI)

--- Lux Lucida/Assets/Scripts/LevelSelect/LevelPad.cs
+++ Lux Lucida/Assets/Scripts/LevelSelect/LevelPad.cs
@@ -9,9 +9,11 @@ public class LevelPad : MonoBehaviour
     [SerializeField]
     private Animator _spotLightAnimator;
     [SerializeField]
-    private bool _touched = false, _Completed = false, _Unlocked = false;
+    private bool _Touched = false, _Completed = false, _Unlocked = false;
     [SerializeField]
     private int _level = 0;
+    [SerializeField]
+    private levelController _LevelController;
 
     void Start()
     {
@@ -22,8 +24,14 @@ public class LevelPad : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-
+        int lastUnlockedLevel = _LevelController._LastUnlockedLevel;
+        if(_level <= lastUnlockedLevel)
+        {
+            _Unlocked = true;
+        }
+        if(_level < lastUnlockedLevel) { 
+            _Completed = true;  
+        }
     }
     private void FixedUpdate()
     {
@@ -32,16 +40,35 @@ public class LevelPad : MonoBehaviour
         {
             Debug.Log("deez");
         }
-        _animator.SetBool("MoreLit", (_touched || _Completed));
+        _animator.SetBool("MoreLit", (_Touched || _Completed));
         _spotLightAnimator.SetBool("Lit", _Completed);
 
     }
     private void OnCollisionEnter(Collision collision)
     {
-        _touched = true;
+        _Touched = true;
+        if(_Unlocked)
+        {
+            _LevelController.SelectLevel(_level);
+            _LevelController.ShowHUD();
+        }
+    }
+    private void OnCollisionStay(Collision collision) {
+        if (_Unlocked && Input.GetKeyDown(KeyCode.E))
+        {
+           
+           
+           _LevelController.StartLevel(_level);
+           
+        }
     }
     private void OnCollisionExit(Collision collision)
     {
-        _touched = false;
+        _Touched = false;
+        if (_Unlocked)
+        {
+            _LevelController.HideHUD();
+        }
     }
+    
 }
