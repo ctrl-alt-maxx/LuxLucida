953576d8e8e5e63a6a75652eac62d6c843246dbd
AsadPug <97375660+AsadPug@users.noreply.github.com>
2024-05-20 17:09:09 -0400

    Tutorial fixes + assets for levers wheel and button

--- Lux Lucida/Assets/Scripts/TutorialController.cs
+++ Lux Lucida/Assets/Scripts/TutorialController.cs
@@ -19,9 +19,6 @@ public class TutorialController : MonoBehaviour
 
     [SerializeField]
     private TutorialZone ZoneEyesOfRa, ZonePlayerJump;
-
-
-    private int _JumpCount = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -63,43 +60,54 @@ public class TutorialController : MonoBehaviour
         if (Input.GetAxis("Horizontal") != 0)
         {
             EventManager.TriggerEvent(EventManager.PossibleEvent.eCloseDialogue, null);
-        }
-        if (ZonePlayerJump.PlayerIsInZone) {  
             _TutorialStep = TutorialStep.PlayerJump;
-            EventManager.TriggerEvent(EventManager.PossibleEvent.eStartDialogue, "Press the [ SPACEBAR ] to jump.");
         }
-        
     }
     private void PlayerJumpUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-           
-            EventManager.TriggerEvent(EventManager.PossibleEvent.eCloseDialogue, null);
-        }
-        if (ZoneEyesOfRa.PlayerIsInZone) { 
-            _TutorialStep = TutorialStep.EyesOfRa;
-            string dialogueText = "You have the Eyes of Ra.\n They are a powerfull light source that you will have to use to complete your quest in lighting up the globe.\n Press [ Q ] to activate/deactivate your glowing eyes.";
-            EventManager.TriggerEvent(EventManager.PossibleEvent.eStartDialogue, dialogueText);
+        if (ZonePlayerJump.PlayerWasInZone)
+        {
+            if (ZonePlayerJump.PlayerJustEnteredZone)
+            {
+                EventManager.TriggerEvent(EventManager.PossibleEvent.eStartDialogue, "Press the [ SPACEBAR ] to jump.");
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                EventManager.TriggerEvent(EventManager.PossibleEvent.eCloseDialogue, null);
+                _TutorialStep = TutorialStep.EyesOfRa;
+            }
         }
 
     }
     private void EyesOfRaUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (ZoneEyesOfRa.PlayerWasInZone)
         {
-            EventManager.TriggerEvent(EventManager.PossibleEvent.eCloseDialogue, null);
+            if (ZoneEyesOfRa.PlayerJustEnteredZone)
+            {
+                string dialogueText = "You have the Eyes of Ra.\n They are a powerfull light source that you will have to use to complete your quest in lighting up the globe.\n Press [ Q ] to activate/deactivate your glowing eyes.";
+                EventManager.TriggerEvent(EventManager.PossibleEvent.eStartDialogue, dialogueText);
+            }
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                EventManager.TriggerEvent(EventManager.PossibleEvent.eCloseDialogue, null);
+                _TutorialStep = TutorialStep.LevelProgress;
+            }
+            
         }
-        if (_GameController.PercentProgress > 25) { 
+        
+        
+    }
+
+    private void LevelProgressUpdate()
+    {   
+        if (_GameController.PercentProgress > 25)
+        {
             string dialogueText = "To complete the level you need to light up 100% of the ground and objects that are darkened in the level.\n The bar in the top will help you keep track how much more there is to do in the level. \n\n But be carefull! Your powers are limited\n In the bottom right of the screen you can see how much energy you have left in your eyes.\n\nHold [ R ] to restart level.";
             _TutorialStep = TutorialStep.LevelProgress;
             EventManager.TriggerEvent(EventManager.PossibleEvent.eStartDialogue, dialogueText);
 
         }
-    }
-
-    private void LevelProgressUpdate()
-    {
-        
     }
     private void InteractableObjectUpdate()
     {
